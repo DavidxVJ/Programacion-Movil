53bf4c097eb523e8a25d9ef6ed87655e329e5c74
DavidxVasquez <143976813+DavidxVasquez@users.noreply.github.com>
2023-10-02 18:22:15 -0600

    Create Programa7.kt

---
+++ Programas en Clase/Programa7.kt
@@ -0,0 +1,27 @@
+//Programa 7
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("Ingresa un valor cualquiera")
+
+    when {
+        scanner.hasNextInt() -> {
+            val valor = scanner.nextInt()
+            println("Has ingresado un entero (int)")
+        }
+        scanner.hasNextFloat() -> {
+            val valor = scanner.nextFloat()
+            println("Has ingresado un número en coma flotante (float)")
+        }
+        scanner.hasNextDouble() -> {
+            val valor = scanner.nextDouble()
+            println("Has ingresado un número en coma flotante doble (double)")
+        }
+        scanner.hasNext() -> {
+            val valor = scanner.next()
+            println("Has ingresado una cadena de caracteres")
+        }
+    }
+    scanner.close()
+}
